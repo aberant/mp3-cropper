54388709a8b97597fd84f11a4350910b5942ef98
aberant <qzzzq1@gmail.com>
2010-03-07 14:17:57 -0600

    tested duration

--- lib/mp3_cropper/models/duration.rb
+++ lib/mp3_cropper/models/duration.rb
@@ -1,5 +1,32 @@
 module Mp3Cropper
   class Duration
+    def self.parse_hh_mm_ss( hhmmss )
+      result = {}
+      time = hhmmss.split(":")
 
+      result[:hours] = time[0].to_i
+      result[:minutes] = time[1].to_i
+      result[:seconds] = time[2].to_i
+      result
+    end
+
+    def self.hh_mm_ss_to_seconds( hhmmss )
+      result = parse_hh_mm_ss( hhmmss )
+      result[:seconds] + (60 * result[:minutes]) + (60 * 60 * result[:hours])
+    end
+
+    def self.new_from_start_stop( start, stop )
+      start_time = hh_mm_ss_to_seconds( start )
+      stop_time = hh_mm_ss_to_seconds( stop )
+
+      new(stop_time - start_time).freeze
+    end
+
+    attr_reader :duration
+    alias to_seconds duration
+
+    def initialize( duration )
+      @duration = duration
+    end
   end
 end
--- spec/models/duration_spec.rb
+++ spec/models/duration_spec.rb
@@ -1,5 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Mp3Cropper::Duration do
+  it "should be able to parse the hh:mm:ss format" do
+    result = Mp3Cropper::Duration.parse_hh_mm_ss( "01:22:54" )
+    result.should == { :hours => 1, :minutes => 22, :seconds => 54 }
+  end
   
+  it "should accept a start and end time in hh:mm:ss format and calculate the duration in seconds" do
+    duration = Mp3Cropper::Duration.new_from_start_stop( "00:00:00", "00:00:35")
+    duration.to_seconds.should == 35
+  end
 end
